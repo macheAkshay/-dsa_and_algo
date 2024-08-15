465368f50c30fc57f7beb1b4f0903b3f427bce77
macheAkshay <akshaymache93@gmail.com>
2024-08-15 16:09:41 +0530

    add print stmt

--- src/main/java/com/git/Main.java
+++ src/main/java/com/git/Main.java
@@ -3,5 +3,6 @@ package com.git;
 public class Main {
     public static void main(String[] args) {
         Service.findFirstNonRepeatedCharacter("akshaky");
+        System.out.println("Hello from bug-fix...!");
     }
 }
