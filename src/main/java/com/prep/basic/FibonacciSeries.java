e02a964f107a340719dc8a81fd741c5d119ee76f
macheAkshay <akshaymache93@gmail.com>
2024-08-28 22:36:30 +0530

    add comments

--- src/main/java/com/prep/basic/FibonacciSeries.java
+++ src/main/java/com/prep/basic/FibonacciSeries.java
@@ -1,7 +1,14 @@
 package com.prep.basic;
 
 /*
-
+    - Java program to print the fibonacci series between given range.
+    - The Fibonacci series is a series of elements where, the previous two elements
+      are added to get the next element, starting with 0 and 1.
+    - Examples of Fibonacci Series in Java
+        Input: N = 9
+        Output: 0 1 1 2 3 5 8 13 21
+        Explanation: Here first term of Fibonacci is 0 and second is 1,
+                     so that 3rd term = first(o) + second(1) etc and so on.
 */
 public class FibonacciSeries {
     public static void main(String[] args) {
